0640dfce7ce4367ff17bd9eab1c1b79a0a318a4d
Grant Riddle <grantdriddle7@gmail.com>
2026-01-22 10:43:58 -0700

    HW01  - problem 3

---
+++ HW1_problem3.py
@@ -0,0 +1,38 @@
+import random
+import matplotlib.pyplot as plt
+
+def func(u):
+	return 1 / ( ( (1 / u) ** 6 + 1) * ( u ** 2) )
+
+above = 0
+below = 0
+
+y = []
+x = []
+
+itr = 1;
+i = 0
+
+while (itr <= 5):
+	xrand = random.random()
+	yrand = random.random()
+
+	if func(xrand) < yrand:
+		above += 1
+	else:
+		below += 1
+	
+	if(i >= 10 ** itr):
+		x.append(itr)
+		y.append(below / i)
+		itr += 0.1
+
+
+	i += 1
+
+print(y[-1])
+plt.plot(x, y)
+plt.xlabel("N (logarithmic)")
+plt.ylabel("integral estimate")
+plt.title("Monte Carlo simulation estimate vs. number of simulations")
+plt.show()
